a168e337c01e45afa0724c16f77f27df6d5c76ab
iuliaco <iuliaco2001@gmail.com>
2022-04-08 23:32:46 +0300

    started producer and consumer stuff, defined register producer and cart

---
+++ tema/consumer.py
@@ -0,0 +1,48 @@
+"""
+This module represents the Consumer.
+
+Computer Systems Architecture Course
+Assignment 1
+March 2021
+"""
+
+from threading import Thread
+
+
+class Consumer(Thread):
+    """
+    Class that represents a consumer.
+    """
+
+    def __init__(self, carts, marketplace, retry_wait_time, **kwargs):
+        """
+        Constructor.
+
+        :type carts: List
+        :param carts: a list of add and remove operations
+
+        :type marketplace: Marketplace
+        :param marketplace: a reference to the marketplace
+
+        :type retry_wait_time: Time
+        :param retry_wait_time: the number of seconds that a producer must wait
+        until the Marketplace becomes available
+
+        :type kwargs:
+        :param kwargs: other arguments that are passed to the Thread's __init__()
+        """
+        Thread.__init__(self, **kwargs)
+
+        self.carts = carts
+        self.marketplace = marketplace
+        self.retry_wait_time = retry_wait_time
+        pass
+
+    def run(self):
+        self.consumer_id = self.marketplace.new_cart()
+        print('Hello consumer! ' , self.carts)
+        #while True:
+            #for product in self.carts:
+                #print(product)
+
+
